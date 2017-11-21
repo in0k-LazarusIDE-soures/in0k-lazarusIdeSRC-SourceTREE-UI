35d89c070e0665ab5375c3925137b66e9380d33a
val <val@kreit.ru>
2017-11-21 18:49:00 +0500

    ВСЁ ПРОПАЛО (((

--- in0k_lazExt_CopyRAST_cmpTree.pas
+++ in0k_lazExt_CopyRAST_cmpTree.pas
@@ -260,7 +260,7 @@ end;
 function tCmp_CopyRAST_Tree._root_get_:tSrcTree_ROOT;
 begin
     {todo: это ВВРЕМЕННАЯ проверка, тут что-то другое надо, для аозможности ГРУПП проектов}
-    {$ifOpt D+}Assert(Assigned(_root_)and(_root_ is tSrcTree_ROOT) ,'_root_ NOT tSrcTree_ROOT');{$endIf}
+    {$ifOpt D+}Assert((not Assigned(_root_)) or (Assigned(_root_)and(_root_ is tSrcTree_ROOT)),'_root_ NOT tSrcTree_ROOT');{$endIf}
     //if root is tSrcTree_ROOT then SrcTREE_imageIndexs_need4Package;
     result:=tSrcTree_ROOT(_root_);
 end;
